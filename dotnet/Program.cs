4da825d42e1ad1e89eee2708dbb91732355e3706
Robson Alves <contato@robsonalves.dev.br>
2025-04-01 16:25:30 -0300

    feat(dotnet): enhance OpenTelemetry configuration with resource attributes and service details

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,13 +1,38 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
 using OpenTelemetry;
 using OpenTelemetry.Exporter;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+
+var serviceName = "RobsonAlves.MyApp";
+var serviceVersion = "1.0.0";
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
+var resourceAttributes = new Dictionary<string, object>
+{
+    ["runtime.os.deescription"] = RuntimeInformation.OSDescription,
+    ["runtime.framework.description"] = RuntimeInformation.FrameworkDescription,
+    ["deployment.environment"] = environment,
+    ["service.name"] = "RobsonAlves.Backend.Microservice.Client",
+    ["service.namespace"] = Assembly.GetExecutingAssembly().GetName().Name ?? "undefined",
+    ["service.version"] = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "undefined"
+};
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+//var serviceName = typeof(Program).Namespace ?? "Unknown";
+//var serviceVersion = typeof(Program).Assembly.GetName().Version?.ToString() ?? "Unknown";
+
 // Add services to the container.
 builder.Services.AddOpenTelemetry()
+    .ConfigureResource(res => {
+        res.AddService(serviceName, serviceVersion);
+        res.AddAttributes(resourceAttributes);
+    })
     .WithTracing(tracing => tracing
         // The rest of your setup code goes here
         .AddOtlpExporter(options =>
@@ -25,6 +50,12 @@ builder.Services.AddOpenTelemetry()
     //.UseOtlpExporter(OtlpExportProtocol.Grpc, new Uri("http://localhost:4317"));
 
 builder.Logging.AddOpenTelemetry(logging => {
+
+    logging.SetResourceBuilder(ResourceBuilder.CreateDefault()
+        .AddService(serviceName, serviceVersion)
+        .AddAttributes(resourceAttributes));
+    logging.IncludeFormattedMessage = true;
+
     // The rest of your setup code goes here
     logging.AddOtlpExporter(options =>
     {
@@ -32,6 +63,9 @@ builder.Logging.AddOpenTelemetry(logging => {
         options.Protocol = OtlpExportProtocol.Grpc;
     });
 });
+
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
